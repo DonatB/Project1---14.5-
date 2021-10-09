801ce3bdddf96bc265f694f48aa7d38e597ee71b
Donat Bajrami <donatbajrami@hotmail.com>
2021-10-10 01:18:00 +0200

    Initial Commit

---
+++ FraktonProject-14.5/Costum Views/Buttons/DBButton.swift
@@ -0,0 +1,36 @@
+//
+//  DBButton.swift
+//  FraktonTestProject
+//
+//  Created by Donat Bajrami on 1.9.21.
+//
+
+import UIKit
+
+class DBButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    convenience init(backgroundColor: UIColor, title: String) {
+        self.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        setTitleColor(.systemBackground, for: .normal)
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ FraktonProject-14.5/Model/UserDataWrapper.swift
@@ -0,0 +1,12 @@
+//
+//  UserDataWrapper.swift
+//  FraktonProject-14.5
+//
+//  Created by Donat Bajrami on 9.10.21.
+//
+
+import Foundation
+
+struct UserDataWrapper <T: Codable>: Codable {
+    let data: T
+}
